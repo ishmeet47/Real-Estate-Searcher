885f8015af7c5af37a5b8f228f899eeb95266709
Willie Pai <PaisWillie@gmail.com>
2023-09-20 11:36:13 -0400

    fix: new property card display

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { IProperty } from '../IProperty.interface';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
 
 @Component({
   selector: 'app-property-card',
+  // template: `<h1>I am a card</h1>`,
   templateUrl: 'property-card.component.html',
+  // styles: ['h1 {font-weight: normal;}']
   styleUrls: ['property-card.component.css'],
 })
 export class PropertyCardComponent {
-  @Input() property: IProperty;
+  @Input() property: IPropertyBase;
+  @Input() hideIcons: boolean;
 }
