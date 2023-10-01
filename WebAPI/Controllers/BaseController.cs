3fd1aaa6f022703e28ef27e3a8a354894ec6fbab
Willie Pai <PaisWillie@gmail.com>
2023-10-01 18:44:01 -0400

    Fix: Temporary fix for getuserid

--- WebAPI/Controllers/BaseController.cs
+++ WebAPI/Controllers/BaseController.cs
@@ -9,7 +9,21 @@ namespace WebAPI.Controllers
     {
         protected int GetUserId()
         {
-            return int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            //var nameIdentifier = ClaimTypes.NameIdentifier;
+            //Console.Write("nameIdentifier: ");
+            //Console.WriteLine(nameIdentifier);
+            //var userId = User.FindFirst(nameIdentifier);
+            //Console.Write("userId: ");
+            //Console.WriteLine(userId);
+            //var userIdValue = userId.Value;
+            //Console.Write("userIdValue: ");
+            //Console.WriteLine(userIdValue);
+            //return int.Parse(userIdValue);
+
+            //var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            // Temporary fix for the above code
+            return 3;
         }
     }
 }
